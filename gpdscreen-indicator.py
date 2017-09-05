4f629c476ab215d3cce6cffa53e08a8cca930791
simone <slysimo@gmail.com>
2017-09-05 22:43:33 +0200

    latest fixes

--- gpdscreen-indicator.py
+++ gpdscreen-indicator.py
@@ -10,15 +10,19 @@
 # Icon author link: https://www.flaticon.com/authors/google
 
 import os
+import time
+import gi
 import signal
 from subprocess import call
+gi.require_version('Gtk', '3.0')
+gi.require_version('AppIndicator3', '0.1')
 from gi.repository import Gtk as gtk
 from gi.repository import AppIndicator3 as appindicator
 
 APPINDICATOR_ID = 'GPDScreenManager'
 
 def main():
-    indicator = appindicator.Indicator.new(APPINDICATOR_ID, os.path.abspath('icons/screen-rotation-button-white.svg'), appindicator.IndicatorCategory.SYSTEM_SERVICES)
+    indicator = appindicator.Indicator.new(APPINDICATOR_ID, get_resource_path('icons/screen-rotation-button-white.svg'), appindicator.IndicatorCategory.SYSTEM_SERVICES)
     indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
     indicator.set_menu(build_menu())
     gtk.main()
@@ -27,26 +31,44 @@ def build_menu():
     menu = gtk.Menu()
 
     # Rotate landscape
-    item_landscape = gtk.MenuItem('Rotate landscape')
+    item_landscape = gtk.ImageMenuItem('Rotate landscape')
+    item_icon = gtk.Image()
+    item_icon.set_from_file(get_resource_path('icons/tablet-with-blank-screen-white.svg'))
+    item_landscape.set_image(item_icon)
+    item_landscape.set_always_show_image(True)
     item_landscape.connect('activate', landscape)
     menu.append(item_landscape)
 
     # Rotate portrait
-    item_portrait = gtk.MenuItem('Rotate Portrait')
+    item_portrait = gtk.ImageMenuItem('Rotate portrait')
+    item_icon = gtk.Image()
+    item_icon.set_from_file(get_resource_path('icons/cell-phone-with-blank-screen-white.svg'))
+    item_portrait.set_image(item_icon)
+    item_portrait.set_always_show_image(True)
     item_portrait.connect('activate', portrait)
     menu.append(item_portrait)
 
     # Restore display size
-    item_displaysize = gtk.MenuItem('Restore display size')
-    item_displaysize.connect('activate', displaysize)
-    menu.append(item_displaysize)
+    #item_displaysize = gtk.MenuItem('Restore display size')
+    #item_displaysize.connect('activate', displaysize)
+    #menu.append(item_displaysize)
+
+    # Restore display size
+    item_resettouch = gtk.ImageMenuItem('Reset touchscreen')
+    item_icon = gtk.Image()
+    item_icon.set_from_file(get_resource_path('icons/synchronization-arrows-white.svg'))
+    item_resettouch.set_image(item_icon)
+    item_resettouch.set_always_show_image(True)
+    item_resettouch.connect('activate', resettouch)
+    menu.append(item_resettouch)
 
     # Quit
-    item_quit = gtk.MenuItem('Quit')
+    item_quit = gtk.ImageMenuItem('Quit')
     item_quit.connect('activate', quit)
     menu.append(item_quit)
 
     menu.show_all()
+
     return menu
 
 def quit(source):
@@ -60,6 +82,17 @@ def portrait(source):
 
 def displaysize(source):
     call(["gpdtouch", "displaysize"])
+
+def resettouch(source):
+    call(("gksudo -- modprobe -r goodix"), shell=True)
+    time.sleep(5) 
+    call(("gksudo -- modprobe goodix"), shell=True)
+
+def get_resource_path(rel_path):
+    dir_of_py_file = os.path.dirname(__file__)
+    rel_path_to_resource = os.path.join(dir_of_py_file, rel_path)
+    abs_path_to_resource = os.path.abspath(rel_path_to_resource)
+    return abs_path_to_resource    
 
 if __name__ == "__main__":
     signal.signal(signal.SIGINT, signal.SIG_DFL)
