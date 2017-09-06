be26ba08dc8da72530c3fd2d9d44b9a500d75f61
Simone Nunzi <slysimo@gmail.com>
2017-09-06 16:01:09 +0000

    Update gpdscreen-indicator.py

--- gpdscreen-indicator.py
+++ gpdscreen-indicator.py
@@ -11,63 +11,89 @@
 
 import os
 import time
-import gi
 import signal
 from subprocess import call
-gi.require_version('Gtk', '3.0')
-gi.require_version('AppIndicator3', '0.1')
-from gi.repository import Gtk as gtk
-from gi.repository import AppIndicator3 as appindicator
+
+bool is_unity;
+
+desktop = getenv("XDG_CURRENT_DESKTOP");                                     
+is_unity = (desktop.toLower() == "unity");    
+is_kde = (desktop.toLower() == "kde");    
+
+if is_unity:
+    import gi
+    gi.require_version('Gtk', '3.0')
+    gi.require_version('AppIndicator3', '0.1')
+    from gi.repository import Gtk as gtk
+    from gi.repository import AppIndicator3 as appindicator
+    
+if !is_unity:
+    from PyQt4 import QtGui
 
 APPINDICATOR_ID = 'GPDScreenManager'
 
 def main():
-    indicator = appindicator.Indicator.new(APPINDICATOR_ID, get_resource_path('icons/screen-rotation-button-white.svg'), appindicator.IndicatorCategory.SYSTEM_SERVICES)
-    indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
-    indicator.set_menu(build_menu())
-    gtk.main()
+    if is_unity:
+        indicator = appindicator.Indicator.new(APPINDICATOR_ID, get_resource_path('icons/screen-rotation-button-white.svg'), appindicator.IndicatorCategory.SYSTEM_SERVICES)
+        indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
+        menu = buid_menu()
+        indicator.set_menu(menu)
+        gtk.main()
+    if !is_unity:
+        indicator = QtGui.QApplication([])
+        icon = QtGui.QSystemTrayIcon(QtGui.QIcon(get_resource_path('icons/screen-rotation-button-white.svg')), indicator)
+        menu = build_menu()
+        icon.setContextMenu(menu)
+        icon.show()
+        app.exec_()
 
 def build_menu():
-    menu = gtk.Menu()
+    if is_unity:
+        menu = gtk.Menu()
 
-    # Rotate landscape
-    item_landscape = gtk.ImageMenuItem('Rotate landscape')
-    item_icon = gtk.Image()
-    item_icon.set_from_file(get_resource_path('icons/tablet-with-blank-screen-white.svg'))
-    item_landscape.set_image(item_icon)
-    item_landscape.set_always_show_image(True)
-    item_landscape.connect('activate', landscape)
-    menu.append(item_landscape)
-
-    # Rotate portrait
-    item_portrait = gtk.ImageMenuItem('Rotate portrait')
-    item_icon = gtk.Image()
-    item_icon.set_from_file(get_resource_path('icons/cell-phone-with-blank-screen-white.svg'))
-    item_portrait.set_image(item_icon)
-    item_portrait.set_always_show_image(True)
-    item_portrait.connect('activate', portrait)
-    menu.append(item_portrait)
-
-    # Restore display size
-    #item_displaysize = gtk.MenuItem('Restore display size')
-    #item_displaysize.connect('activate', displaysize)
-    #menu.append(item_displaysize)
-
-    # Restore display size
-    item_resettouch = gtk.ImageMenuItem('Reset touchscreen')
-    item_icon = gtk.Image()
-    item_icon.set_from_file(get_resource_path('icons/synchronization-arrows-white.svg'))
-    item_resettouch.set_image(item_icon)
-    item_resettouch.set_always_show_image(True)
-    item_resettouch.connect('activate', resettouch)
-    menu.append(item_resettouch)
-
-    # Quit
-    item_quit = gtk.ImageMenuItem('Quit')
-    item_quit.connect('activate', quit)
-    menu.append(item_quit)
-
-    menu.show_all()
+        # Rotate landscape
+        item_landscape = gtk.ImageMenuItem('Rotate landscape')
+        item_icon = gtk.Image()
+        item_icon.set_from_file(get_resource_path('icons/tablet-with-blank-screen-white.svg'))
+        item_landscape.set_image(item_icon)
+        item_landscape.set_always_show_image(True)
+        item_landscape.connect('activate', landscape)
+        menu.append(item_landscape)
+    
+        # Rotate portrait
+        item_portrait = gtk.ImageMenuItem('Rotate portrait')
+        item_icon = gtk.Image()
+        item_icon.set_from_file(get_resource_path('icons/cell-phone-with-blank-screen-white.svg'))
+        item_portrait.set_image(item_icon)
+        item_portrait.set_always_show_image(True)
+        item_portrait.connect('activate', portrait)
+        menu.append(item_portrait)
+    
+        # Restore display size
+        #item_displaysize = gtk.MenuItem('Restore display size')
+        #item_displaysize.connect('activate', displaysize)
+        #menu.append(item_displaysize)
+    
+        # Restore display size
+        item_resettouch = gtk.ImageMenuItem('Reset touchscreen')
+        item_icon = gtk.Image()
+        item_icon.set_from_file(get_resource_path('icons/synchronization-arrows-white.svg'))
+        item_resettouch.set_image(item_icon)
+        item_resettouch.set_always_show_image(True)
+        item_resettouch.connect('activate', resettouch)
+        menu.append(item_resettouch)
+    
+        # Quit
+        item_quit = gtk.ImageMenuItem('Quit')
+        item_quit.connect('activate', quit)
+        menu.append(item_quit)
+    
+        menu.show_all()
+        
+    if !is_unity:
+        menu = QtGui.QMenu()
+        
+        menu.addAction("Quit", QtGui.qApp.quit)
 
     return menu
 
