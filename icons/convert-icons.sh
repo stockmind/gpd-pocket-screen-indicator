67de7cbcfe776b25036a0960fc7c0270af69fca3
Simone <slysimo@gmail.com>
2017-09-15 20:08:19 +0000

    Convert icons to 32px

---
+++ icons/convert-icons.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+SIZE=32
+
+inkscape -z -e screen-rotation-button-white.png -w "$SIZE" -h "$SIZE" screen-rotation-button-white.svg
+inkscape -z -e close-button-white.png -w "$SIZE" -h "$SIZE" close-button-white.svg
+inkscape -z -e cell-phone-with-blank-screen-white.png -w "$SIZE" -h "$SIZE" cell-phone-with-blank-screen-white.svg
+inkscape -z -e synchronization-arrows-white.png -w "$SIZE" -h "$SIZE" synchronization-arrows-white.svg
+inkscape -z -e tablet-with-blank-screen-white.png -w "$SIZE" -h "$SIZE" tablet-with-blank-screen-white.svg
