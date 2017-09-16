b26ef297ab46e1cb0c6534ff66571d60e10b25ad
Simone <slysimo@gmail.com>
2017-09-16 00:32:28 +0000

    black icon

--- icons/convert-icons.sh
+++ icons/convert-icons.sh
@@ -2,6 +2,7 @@
 
 SIZE=32
 
+inkscape -z -e screen-rotation-button-black.png -w "$SIZE" -h "$SIZE" screen-rotation-button-black.svg
 inkscape -z -e screen-rotation-button-white.png -w "$SIZE" -h "$SIZE" screen-rotation-button-white.svg
 inkscape -z -e close-button-white.png -w "$SIZE" -h "$SIZE" close-button-white.svg
 inkscape -z -e cell-phone-with-blank-screen-white.png -w "$SIZE" -h "$SIZE" cell-phone-with-blank-screen-white.svg
