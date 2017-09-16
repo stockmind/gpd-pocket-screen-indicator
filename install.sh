3ca26f829fb9e6bdea56463aa2bc5756f7e459da
stockmind <slysimo@gmail.com>
2017-09-16 14:47:10 +0200

    Duplicated commands

--- install.sh
+++ install.sh
@@ -49,10 +49,6 @@ systemctl start gpdscreen.service
 cp gpdscreen-indicator.py /usr/local/sbin/gpdscreen-indicator
 chmod +x /usr/local/sbin/gpdscreen-indicator
 
-cp xsession/99-gpdscreen-indicator /etc/X11/Xsession.d/99-gpdscreen-indicator
-
-chmod +x /etc/X11/Xsession.d/99-gpdscreen-indicator
-
 # Install icons
 mkdir -p /usr/local/share/gpdscreen-indicator/icons
 cp icons/*.png /usr/local/share/gpdscreen-indicator/icons/
