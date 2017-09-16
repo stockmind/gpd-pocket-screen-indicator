ad168585a66da7734a5c3c62ba5c3763f9bf536c
stockmind <slysimo@gmail.com>
2017-09-16 03:36:42 +0200

    Create problem-reporting.sh

---
+++ problem-reporting.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+if [ $EUID != 0 ]; then
+    sudo "$0" "$@"
+    exit $?
+fi
+
+# GPD Pocket system environment check
+# Author: Simone Roberto Nunzi aka stockmind
+# Problem reporting script
+
+echo "**Kernel:** $(uname -r)"
+echo "**Desktop Environment:** $XDG_CURRENT_DESKTOP"
+echo "**Display:** $DISPLAY"
+echo "**Monitor:** $(xrandr | grep DSI1)"
+echo "**Scripts:** " $(ls /usr/local/sbin/ | tr "\n" " ")
+echo "**EFI:** " $(dmesg | grep "EFI v")
+echo "**EFI** " $(dmidecode | grep "Release")
