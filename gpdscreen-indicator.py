598ae6409029ddb26c61319dddb6b645978dfcfe
stockmind <slysimo@gmail.com>
2018-02-07 06:10:32 +0100

    config file tests

--- gpdscreen-indicator.py
+++ gpdscreen-indicator.py
@@ -13,6 +13,7 @@ import os
 import time
 import signal
 from subprocess import call
+import ConfigParser
 
 import gi
 gi.require_version('Gtk', '3.0')
@@ -21,9 +22,41 @@ from gi.repository import Gtk as gtk
 from gi.repository import AppIndicator3 as appindicator
 
 APPINDICATOR_ID = 'GPDScreenManager'
+configfile = "/etc/gpdscreen-indicator/config"
+Config = ConfigParser.ConfigParser()
+
+def getconfig(section):
+    dict1 = {}
+    options = Config.options(section)
+    for option in options:
+        try:
+            dict1[option] = Config.get(section, option)
+            if dict1[option] == -1:
+                DebugPrint("skip: %s" % option)
+        except:
+            print("exception on %s!" % option)
+            dict1[option] = None
+    return dict1
+
+def setconfig(section,key,value):
+    Config.set(section,key,value)
+    with open(configfile, 'wb') as file:
+        config.write(file)
 
 def main():
-    indicator = appindicator.Indicator.new(APPINDICATOR_ID, get_resource_path('/usr/local/share/gpdscreen-indicator/icons/screen-rotation-button-white.svg'), appindicator.IndicatorCategory.SYSTEM_SERVICES)
+    # Read config
+    Config.read(configfile)
+    maincolor = getconfig("icons")['maincolor'] # white - black
+    menucolor = getconfig("icons")['menucolor'] # white - black
+
+    if maincolor = None:
+        maincolor = "white"
+
+    if menucolor = None:
+        menucolor = "white"
+
+    # Init indicator
+    indicator = appindicator.Indicator.new(APPINDICATOR_ID, get_resource_path('/usr/local/share/gpdscreen-indicator/icons/screen-rotation-button-{}.svg'.format(maincolor)), appindicator.IndicatorCategory.SYSTEM_SERVICES)
     indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
     menu = build_menu()
     indicator.set_menu(menu)
@@ -35,7 +68,7 @@ def build_menu():
     # Rotate landscape
     item_landscape = gtk.ImageMenuItem('Rotate landscape')
     item_icon = gtk.Image()
-    item_icon.set_from_file(get_resource_path('/usr/local/share/gpdscreen-indicator/icons/tablet-with-blank-screen-white.svg'))
+    item_icon.set_from_file(get_resource_path('/usr/local/share/gpdscreen-indicator/icons/tablet-with-blank-screen-{}.svg'.format(menucolor)))
     item_landscape.set_image(item_icon)
     item_landscape.set_always_show_image(True)
     item_landscape.connect('activate', landscape)
@@ -44,7 +77,7 @@ def build_menu():
     # Rotate portrait
     item_portrait = gtk.ImageMenuItem('Rotate portrait')
     item_icon = gtk.Image()
-    item_icon.set_from_file(get_resource_path('/usr/local/share/gpdscreen-indicator/icons/cell-phone-with-blank-screen-white.svg'))
+    item_icon.set_from_file(get_resource_path('/usr/local/share/gpdscreen-indicator/icons/cell-phone-with-blank-screen-{}.svg'.format(menucolor)))
     item_portrait.set_image(item_icon)
     item_portrait.set_always_show_image(True)
     item_portrait.connect('activate', portrait)
@@ -53,7 +86,7 @@ def build_menu():
     # Rotate inverted portrait
     item_portrait = gtk.ImageMenuItem('Rotate inverse portrait')
     item_icon = gtk.Image()
-    item_icon.set_from_file(get_resource_path('/usr/local/share/gpdscreen-indicator/icons/cell-phone-with-blank-screen-white.svg'))
+    item_icon.set_from_file(get_resource_path('/usr/local/share/gpdscreen-indicator/icons/cell-phone-with-blank-screen-{}.svg'.format(menucolor)))
     item_portrait.set_image(item_icon)
     item_portrait.set_always_show_image(True)
     item_portrait.connect('activate', invertedportrait)
@@ -67,7 +100,7 @@ def build_menu():
     # Restore display size
     item_resettouch = gtk.ImageMenuItem('Reset touchscreen')
     item_icon = gtk.Image()
-    item_icon.set_from_file(get_resource_path('/usr/local/share/gpdscreen-indicator/icons/synchronization-arrows-white.svg'))
+    item_icon.set_from_file(get_resource_path('/usr/local/share/gpdscreen-indicator/icons/synchronization-arrows-{}.svg'.format(menucolor)))
     item_resettouch.set_image(item_icon)
     item_resettouch.set_always_show_image(True)
     item_resettouch.connect('activate', resettouch)
@@ -83,6 +116,20 @@ def build_menu():
     item_highdpi.connect('activate', highdpi)
     menu.append(item_highdpi)
 
+    # Preferences menu
+    preferencesmenu = gtk.Menu()
+    item_preferences = gtk.ImageMenuItem('Preferences')
+    item_preferences.set_submenu(preferencesmenu)
+        # Theme White
+        item_preferences_themewhite = gtk.ImageMenuItem('White Theme')
+        item_preferences_themewhite.connect('activate', themewhite)
+        preferencesmenu.append(item_preferences_themewhite)
+        # Theme Black
+        item_preferences_themeblack = gtk.ImageMenuItem('Black Theme')
+        item_preferences_themeblack.connect('activate', themeblack)
+        preferencesmenu.append(item_preferences_themeblack)
+    menu.append(item_preferences)
+
     # Quit
     item_quit = gtk.ImageMenuItem('Quit')
     item_quit.connect('activate', quit)
@@ -93,6 +140,7 @@ def build_menu():
     return menu
 
 def quit(*source):
+    Config.quit()
     gtk.main_quit()
 
 def landscape(*source):
@@ -115,6 +163,21 @@ def highdpi(*source):
 
 def normaldpi(*source):
     call(["gpdscreen", "normaldpi"])
+
+def themewhite(*source):
+    setconfig("icons","maincolor","white")
+    setconfig("icons","menucolor","white")
+    call(["gpdscreen-indicator"])
+    Config.quit()
+    gtk.main_quit()
+
+def themeblack(*source):
+    setconfig("icons","maincolor","black")
+    setconfig("icons","menucolor","black")
+    call(["gpdscreen-indicator"])
+    Config.quit()
+    gtk.main_quit()
+
 
 def get_resource_path(rel_path):
     #dir_of_py_file = os.path.dirname(__file__)
